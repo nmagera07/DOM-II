2d3981f05f30211a6cf0670ffa21d01df1a4a280
Nate Magera <nmagera07@gmail.com>
2019-05-14 16:17:13 -0400

    finished some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', (event) => {
+    event.target.style.color = "Cyan";
+    console.log('mouseover')
+})
+nav.addEventListener('mouseout', (event) => {
+    event.target.style.color = "black"
+    console.log('mouseout')
+})
+
+// const headerImg = document.querySelectorAll('img');
+// headerImg.addEventListener('click', event => {
+//     event.target.style.width = "200px"
+//     console.log('click')
+// })
+
+const nodeList = document.querySelectorAll('img')
+
+for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].addEventListener('click', function() {
+        nodeList[i].style.width = "200px"
+    })
+}
