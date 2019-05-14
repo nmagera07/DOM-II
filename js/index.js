8a9628e5af027c62d185b13554bec5bdd6664e03
Nate Magera <nmagera07@gmail.com>
2019-05-14 17:45:36 -0400

    finished the MVP

--- js/index.js
+++ js/index.js
@@ -2,23 +2,57 @@
 const nav = document.querySelector('.nav');
 nav.addEventListener('mouseover', (event) => {
     event.target.style.color = "Cyan";
-    console.log('mouseover')
+    event.preventDefault()
 })
 nav.addEventListener('mouseout', (event) => {
     event.target.style.color = "black"
-    console.log('mouseout')
 })
-
-// const headerImg = document.querySelectorAll('img');
-// headerImg.addEventListener('click', event => {
-//     event.target.style.width = "200px"
-//     console.log('click')
-// })
 
 const nodeList = document.querySelectorAll('img')
 
 for (let i = 0; i < nodeList.length; i++) {
     nodeList[i].addEventListener('click', function() {
-        nodeList[i].style.width = "200px"
+        nodeList[i].style.width = "75%"
     })
 }
+for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].addEventListener('dblclick', function() {
+        nodeList[i].style.width = "100%"
+    })
+}
+
+const titles = document.querySelectorAll('.btn')
+
+for (let i = 0; i < titles.length; i++) {
+    titles[i].addEventListener('wheel', function() {
+        titles[i].style.color = "Maroon"
+        titles[i].style.backgroundColor = "Yellow"
+    })
+}
+
+for (let i = 0; i < titles.length; i++) {
+    titles[i].addEventListener('pointerout', function() {
+        titles[i].style.color = "white"
+        titles[i].style.backgroundColor = "#17A2B8"
+    })
+}
+
+window.addEventListener('load', event => {
+    alert('Page is fully loaded!')
+})
+
+const textContent = document.querySelector('.home .content-section .text-content')
+textContent.addEventListener('copy', event => {
+    event.target.style.backgroundColor = "LightCyan"
+})
+textContent.addEventListener('click', event => {
+    event.target.style.backgroundColor = "white"
+})
+
+const footerContent = document.querySelector('footer')
+footerContent.addEventListener('mouseenter', event => {
+    event.target.style.backgroundColor = "white"
+})
+footerContent.addEventListener('mouseleave', event => {
+    event.target.style.backgroundColor = "#FFEBCD"
+})
